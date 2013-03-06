5238150d8c90cc6dec94f1bcade0ca229f2546c4
Noah Davis <ndavis@tty72.com>
2013-03-06 13:27:31 -0500

    Finish Exchange() importer/exporter code

--- qemutubes/models/importexport.py
+++ qemutubes/models/importexport.py
@@ -3,10 +3,31 @@ from xml.dom import minidom
 import StringIO
 
 class Exchange(object):
-    def __init__(self, Base):
-        self.tables = Base.metadata.sorted_tables        
+    """ Object used to export and import DB data to and from XML
+    Export workflow:
+       x=Exchange(metadata, ignlist=['ignored_table', 'some_other_ignored_table'])
+       x.export_xml() # Builds internal XML doc from DB data
+       x.write_xml(fd) # Write XML to a file object
+    Import workflow:
+       x=Exchange(metadata)
+       x.import_xml('file.xml') # Import XML file
+       x.write_db() # Write imported data to DB
+       """
+    def __init__(self, metadata, ignlist=None):
+        """ Initialize an Exchanger with optional list of tables to ignore
+        metadata - The metadata for the tables we care about
+        ignlist - list of table names to ignore """
+        self.tables = list(metadata.sorted_tables)
+        if ignlist:
+            for table in list(self.tables):
+                print table.name, str(ignlist)
+                if table.name in ignlist:
+                    print "Ignoring: %s", table.name
+                    self.tables.remove(table)
 
-    def export(self):
+    # Exporter code
+    def export_xml(self):
+        """ Create XML doc from DB data """
         self.doc = minidom.Document()
         root = self.doc.createElement('qtubesexport')
         root.setAttribute('version', dbversion)
@@ -18,9 +39,14 @@ class Exchange(object):
             self.export_table(e, t)
 
     def export_table(self, parent, table):
+        """ Create XML entries for each row in a given table
+        parent - parent XML node to graft to
+        table - table to pull data from
+        """
         engine = table.metadata.bind
         res = engine.execute(table.select()).fetchall()
         for row in res:
+            rowel = self.doc.createElement('row')
             for (col, val) in row.items():
                 e = self.doc.createElement(col)
                 if val is None:
@@ -30,89 +56,78 @@ class Exchange(object):
                 else:
                     v = self.doc.createTextNode(str(val))
                     e.appendChild(v)
-                parent.appendChild(e)
+                rowel.appendChild(e)
+            parent.appendChild(rowel)
 
-    def write(self, fd, **kw):
+    def write_xml(self, fd, **kw):
+        """ Write XML data to a file-like object.
+        fd - file-like object or string containing file path
+        **kw - passed directly to minidom.Document.writexml()
+        """
         if type(fd) is not file:
             fd = open(fd, 'w')
         self.doc.writexml(fd, **kw)
 
-##########################################
-class Importer(object):
-    """ XML to DB Importer class """
-    def __init__(self, fd):
-        if type(fd) is not file:
-            fd = open(fd,'r')
-        self.doc = minidom.parse(fd)
-        self.root = self.doc.documentElement
-        if (not self.root.attributes.has_key('version') or
-            self.root.tagName != 'qtubesdump'):
-            raise Exception('Unknown document type')
-        if self.root.attributes['version'].value != Exporter.version:
-            raise Exception('Incorrect version: %s' %
-                            root.attributes['version'].value)
-        self.vdes = {}
-        self.machines = {}
-        self.parse_xml()
-
-    def parse_xml(self):
-        """ Parse DB fields from XML """
-        vdeents = self.root.getElementsByTagName('vdeswitch')
-        machines = self.root.getElementsByTagName('machine')
-        for v in vdeents:
-            (name, data) = self.parse_vde(v)
-            self.vdes[name] = data
-        for m in machines:
-            (name, data) = self.parse_machine(m)
-            self.machines[name] = data
-
-    def parse_vde(self, vde):
-        saved_cols = ['sock', 'mgmt', 'tap', 'mode', 'group',
-                      'rcfile', 'ports', 'hub', 'fstp', 'macaddr']
-        name = vde.attributes['name'].value
-        data = self.parse_cols(vde, saved_cols)
-        return (name, data)
-
-    def parse_machine(self, machine):
-        saved_cols = ['cpu', 'machtype', 'mem', 'vncport',
-                      'conport', 'netnone']
-        name = machine.attributes['name'].value
-        data = self.parse_cols(machine, saved_cols)
-        data['drives']=[]
-        data['nets']=[]
-        drives = machine.getElementsByTagName('drive')
-        for drive in drives:
-            data['drives'].append(self.parse_drive(drive))
-        nets = machine.getElementsByTagName('net')
-        for net in nets:
-            data['nets'].append(self.parse_net(net))
-        return (name, data)
-
-    def parse_drive(self, drive):
-        saved_cols = ['filepath', 'interface', 'media', 'bus', 'unit', 'ind',
-                      'cyls', 'heads', 'secs', 'trans', 'snapshot', 'cache',
-                      'aio', 'ser']
-        return self.parse_cols(drive, saved_cols)
-
-    def parse_net(self, net):
-        saved_cols = ['ntype', 'vlan', 'nicmodel', 'macaddr',
-                      'port', 'script', 'downscript', 'ifname']
-        print net.tagName
-        name = net.attributes['name'].value
-        data = self.parse_cols(net, saved_cols)
-        data['name'] = name
-        return data
-
-    def parse_cols(self, node, cols):
-        """ Parse all children of node matching one of cols
-        node - parent node to search
-        cols - list of node/columns
-        returns dict with cols as keys
+    # Importer code
+    def import_xml(self, fd):
+        """ Import XML data from file-like object.
+        fd - File-like object of string containing path name
         """
-        ret = {}
-        for c in cols:
-            ret[c] = self.get_data(node.getElementsByTagName(c)[0])
-        return ret
+        if type(fd) is not file:
+            fd = open(fd, 'r')
+        self.doc = minidom.parse(fd)
+        root = self.doc.documentElement
+        if (not root.attributes.has_key('version') or
+            root.tagName != 'qtubesexport'):
+            raise Exception('Unknown document type')
+        if root.attributes['version'].value != dbversion:
+            raise Exception('Incorrect version: %s != %s' %
+                            root.attributes['version'].value,
+                            dbversion)
+
+    def parse_tables(self):
+        """ Parse table entries from XML document """
+        root = self.doc.documentElement
+        dbdata = {}
+        for table in [n for n in root.childNodes 
+                      if n.nodeType==n.ELEMENT_NODE]:
+            rows = self.parse_rows(table)
+            dbdata[table.tagName] = rows
+        return dbdata
+
+    def parse_rows(self, table):
+        """ Parse row data from XML document
+        table - XML node for a given table
+        """
+        rowlist = []
+        for row in [n for n in table.childNodes 
+                    if n.nodeType==n.ELEMENT_NODE]:
+            coldict = {}
+            for col in [n for n in row.childNodes 
+                        if n.nodeType==n.ELEMENT_NODE]:
+                if col.hasAttribute('none'):
+                    val = None
+                elif col.hasAttribute('bool'):
+                    val = True if col.getAttribute('bool') == 'true' else False
+                else:
+                    val = self.get_data(col)
+                coldict[col.tagName] = val
+            rowlist.append(coldict)
+        return rowlist
+
+    def write_db(self):
+        """ Insert parsed data into database """
+        data = self.parse_tables()
+        for table in self.tables:
+            conn = table.metadata.bind.connect()
+            try:
+                rows = data[table.name]
+            except KeyError:
+                pass # Data not in XML file, skip it
+            else:
+                conn.execute(table.insert(), rows)
+            finally:
+                conn.close()
 
     def get_data(self, node):
         """ Get text data from a node. Whitespace is collapsed between nodes """
@@ -122,3 +137,4 @@ class Importer(object):
             if n.nodeType == n.TEXT_NODE:
               val.append(n.data.strip())
         return ' '.join(val)
+
