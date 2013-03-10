bc4286dca148b3b2699731641b9fd7e989eb9bca
Noah Davis <ndavis@tty72.com>
2013-03-10 12:15:25 -0400

    DDMenu - make sub-menus lists instead of dicts to preserve ordering

--- qemutubes/widgets/mainmenu.py
+++ qemutubes/widgets/mainmenu.py
@@ -2,12 +2,12 @@ from . import ddmenu
 
 class MainMenu(ddmenu.DDMenu):
     id = 'main_menu'
-    dbmenu = { 'export': { 'label': 'Export', 'target': '#', },
-               'import': { 'label': 'Import', 'target': '#', },
-               }
-    toolsmenu = { 'dbmenu': { 'label': 'DB', 'target': '#',
-                              'submenu': dbmenu, } 
-                  }
+    dbmenu = [{ 'label': 'Export', 'target': '#', },
+              { 'label': 'Import', 'target': '#', },
+              ]
+    toolsmenu = [{ 'label': 'DB', 'target': '#',
+                   'submenu': dbmenu, } 
+                  ]
     menu = { 'main': { 'label': 'Main', 'target': '/' },
              'tools': { 'label': 'Tools', 'target': '#',
                         'submenu': toolsmenu, }
